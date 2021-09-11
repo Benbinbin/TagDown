482cc395431c79a813d7e99d548814b37637e8b8
Ben <3126319718@qq.com>
2021-09-12 00:05:10 +0800

    add popup menu.

--- src/popup/components/BrowserPage.vue
+++ src/popup/components/BrowserPage.vue
@@ -22,14 +22,44 @@
         @set-group-color="groupColor = $event"
       />
     </div>
-    <div class="flex-grow all-center">
+    <div class="flex-grow all-center space-x-2 space-y-2">
       <component :is="browserContentComponent" />
       <button
         class="p-1 text-white bg-green-400 rounded"
-        @click="showRenameFolderModal=true"
+        @click="showRenameFolderModal = true"
       >
         Rename Folder
       </button>
+      <button
+        title="show more setting menu"
+        class="p-0.5 text-gray-300 hover:text-gray-600 hover:bg-gray-200 rounded-sm"
+        @click="showBrowserPopupMenuHandler('tag1', $event)"
+      >
+        <svg
+          class="w-4 h-4"
+          viewBox="0 0 50 50"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M27.6875 37.3438C27.6875 37.8073 27.55 38.2604 27.2925 38.6459C27.035 39.0313 26.6689 39.3317 26.2407 39.5091C25.8124 39.6865 25.3412 39.7329 24.8865 39.6425C24.4319 39.552 24.0142 39.3288 23.6865 39.001C23.3587 38.6733 23.1355 38.2556 23.045 37.801C22.9546 37.3463 23.001 36.8751 23.1784 36.4468C23.3558 36.0186 23.6562 35.6525 24.0416 35.395C24.4271 35.1375 24.8802 35 25.3438 35C25.9651 35.0007 26.5609 35.2479 27.0002 35.6873C27.4396 36.1266 27.6868 36.7224 27.6875 37.3438ZM25.3438 14.6875C25.8073 14.6875 26.2604 14.55 26.6459 14.2925C27.0313 14.035 27.3317 13.6689 27.5091 13.2407C27.6865 12.8124 27.7329 12.3412 27.6425 11.8865C27.552 11.4319 27.3288 11.0142 27.001 10.6865C26.6733 10.3587 26.2556 10.1355 25.801 10.045C25.3464 9.9546 24.8751 10.001 24.4468 10.1784C24.0186 10.3558 23.6525 10.6562 23.395 11.0416C23.1375 11.4271 23 11.8802 23 12.3438C23.0007 12.9651 23.2479 13.5609 23.6873 14.0002C24.1267 14.4396 24.7224 14.6868 25.3438 14.6875ZM25.3438 22.5C24.8802 22.5 24.4271 22.6375 24.0416 22.895C23.6562 23.1525 23.3558 23.5186 23.1784 23.9468C23.001 24.3751 22.9546 24.8464 23.045 25.301C23.1355 25.7556 23.3587 26.1733 23.6865 26.501C24.0142 26.8288 24.4319 27.052 24.8865 27.1425C25.3412 27.2329 25.8124 27.1865 26.2407 27.0091C26.6689 26.8317 27.035 26.5313 27.2925 26.1459C27.55 25.7604 27.6875 25.3073 27.6875 24.8438C27.6868 24.2224 27.4396 23.6267 27.0002 23.1873C26.5609 22.7479 25.9651 22.5007 25.3438 22.5Z"
+          />
+        </svg>
+      </button>
+      <div
+        v-show="showBrowserPopupMenu"
+        class="fixed inset-0 z-10"
+        @click="showBrowserPopupMenu = false"
+      >
+        <div
+          ref="menuWrapper"
+          class="absolute z-20"
+        >
+          <BrowserPopupMenu
+            :item-type="'bookmark'"
+          />
+        </div>
+      </div>
       <InputModal
         v-if="showRenameFolderModal"
         v-model:show="showRenameFolderModal"
@@ -81,6 +111,7 @@ import BrowserTree from './Browser/BrowserTree.vue';
 import BrowserMenu from './Browser/BrowserMenu.vue';
 import BrowserMenuPin from './Browser/BrowserMenuPin.vue';
 import InputModal from './Modal/InputModal.vue';
+import BrowserPopupMenu from './Modal/BrowserPopupMenu.vue';
 
 export default {
   components: {
@@ -91,6 +122,7 @@ export default {
     BrowserMenu,
     BrowserMenuPin,
     InputModal,
+    BrowserPopupMenu,
   },
   setup() {
     // browser page setting
@@ -126,6 +158,24 @@ export default {
       console.log(obj);
     };
 
+    // browser popup menu
+    const menuWrapper = ref(null);
+    const showBrowserPopupMenu = ref(false);
+    const showBrowserPopupMenuHandler = (value, event) => {
+      console.log(value);
+      const target = event.currentTarget;
+      console.log(target);
+      console.log(menuWrapper.value);
+      if (!target || !menuWrapper.value) return;
+      const left = target.offsetLeft + 20;
+      const top = target.offsetTop + 20;
+
+      menuWrapper.value.style.left = `${left}px`;
+      menuWrapper.value.style.top = `${top}px`;
+      showBrowserPopupMenu.value = true;
+      console.log(showBrowserPopupMenu.value);
+    };
+
     return {
       browserType,
       browserMode,
@@ -139,6 +189,9 @@ export default {
       multiSwitch,
       showRenameFolderModal,
       getRenameResultHandler,
+      menuWrapper,
+      showBrowserPopupMenu,
+      showBrowserPopupMenuHandler,
     };
   },
 };
@@ -146,6 +199,10 @@ export default {
 
 <style lang="scss" scoped>
 .all-center {
-  @apply flex justify-center items-center
+  @apply flex justify-center items-center;
 }
 </style>
+
+.all-center {
+  @apply flex justify-center items-center;
+}
